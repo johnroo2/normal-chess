53b607ddb013bcbe4ff8c33fe2726bacaabbc8af
John Liu <johnliu@v1041-wn-rt-b-202-10.campus-dynamic.uwaterloo.ca>
2023-11-06 18:58:14 -0500

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html",
+  purge: ["./index.html",
   "./src/**/*.{js,ts,jsx,tsx}",],
+  darkMode:false,
   theme: {
+    extend: {},
+  },
+  variants:{
     extend: {},
   },
   plugins: [],
