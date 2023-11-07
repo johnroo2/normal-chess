1b2f9128c17a8f086fd90645f47874cf6c73c1dd
John Liu <johnliu@v1041-wn-rt-b-202-10.campus-dynamic.uwaterloo.ca>
2023-11-06 19:02:01 -0500

    postcss/tailwind install

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  purge: ["./index.html",
-  "./src/**/*.{js,ts,jsx,tsx}",],
-  darkMode:false,
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
